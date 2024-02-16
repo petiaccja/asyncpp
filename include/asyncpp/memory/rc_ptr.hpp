779a300f3ece1ae9198b42e00c2b52d816e9b213
Péter Kardos <kardospeter1994@hotmail.com>
2024-02-16 22:47:44 +0100

    add rc_ptr

---
+++ include/asyncpp/memory/rc_ptr.hpp
@@ -0,0 +1,127 @@
+#pragma once
+
+#include <atomic>
+#include <cassert>
+#include <concepts>
+#include <utility>
+
+
+namespace asyncpp {
+
+class rc_from_this {
+    template <std::derived_from<rc_from_this> T, class Deleter>
+    friend class rc_ptr;
+
+    std::atomic_size_t m_rc = 0;
+};
+
+
+template <class T>
+    requires requires(T* object) { object->destroy(); }
+struct rc_default_delete {
+    void operator()(T* object) const {
+        object->destroy();
+    }
+};
+
+
+template <std::derived_from<rc_from_this> T, class Deleter = rc_default_delete<T>>
+class rc_ptr {
+public:
+    rc_ptr() noexcept(std::is_nothrow_constructible_v<Deleter>)
+        : m_ptr(nullptr) {}
+
+    rc_ptr(T* ptr, Deleter deleter = {}) noexcept(std::is_nothrow_move_constructible_v<Deleter>)
+        : m_ptr(ptr),
+          m_deleter(std::move(deleter)) {
+        increment();
+    }
+
+    rc_ptr(rc_ptr&& other) noexcept(std::is_nothrow_move_constructible_v<Deleter>)
+        : m_ptr(std::exchange(other.m_ptr, nullptr)),
+          m_deleter(std::move(other.m_deleter)) {}
+
+    rc_ptr(const rc_ptr& other) noexcept(std::is_nothrow_copy_constructible_v<Deleter>)
+        : m_ptr(other.m_ptr),
+          m_deleter(other.m_deleter) {
+        increment();
+    }
+
+    rc_ptr& operator=(rc_ptr&& other) noexcept(std::is_nothrow_move_constructible_v<Deleter>) {
+        m_ptr = std::exchange(other.m_ptr, nullptr);
+        m_deleter = std::move(other.m_deleter);
+        return *this;
+    }
+
+    rc_ptr& operator=(const rc_ptr& other) {
+        if (this != &other) {
+            decrement();
+            m_ptr = other.m_ptr;
+            m_deleter = other.m_deleter;
+            increment();
+        }
+        return *this;
+    }
+
+    ~rc_ptr() {
+        decrement();
+    }
+
+    T* get() const noexcept {
+        return m_ptr;
+    }
+
+    T& operator*() const noexcept {
+        assert(m_ptr);
+        return *m_ptr;
+    }
+
+    T* operator->() const noexcept {
+        return m_ptr;
+    }
+
+    size_t use_count() const noexcept {
+        if (m_ptr) {
+            return m_ptr->rc_from_this::m_rc.load(std::memory_order_relaxed);
+        }
+        return 0;
+    }
+
+    bool unique() const noexcept {
+        return use_count() == 1;
+    }
+
+    explicit operator bool() const noexcept {
+        return m_ptr != nullptr;
+    }
+
+    auto operator<=>(const rc_ptr& other) const noexcept {
+        return m_ptr <=> other.m_ptr;
+    }
+
+    auto operator==(const rc_ptr& other) const noexcept {
+        return m_ptr == other.m_ptr;
+    }
+
+private:
+    void increment() const noexcept {
+        if (m_ptr) {
+            m_ptr->rc_from_this::m_rc.fetch_add(1, std::memory_order_relaxed);
+        }
+    }
+
+    void decrement() const {
+        if (m_ptr) {
+            const auto count = m_ptr->rc_from_this::m_rc.fetch_sub(1, std::memory_order_relaxed) - 1;
+            if (count == 0) {
+                m_deleter(m_ptr);
+            }
+        }
+    }
+
+private:
+    T* m_ptr;
+    Deleter m_deleter;
+};
+
+} // namespace asyncpp
---
+++ test/memory/test_rc_ptr.cpp
@@ -0,0 +1,129 @@
+#include <asyncpp/memory/rc_ptr.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+
+using namespace asyncpp;
+
+
+struct managed : rc_from_this {
+    void destroy() {
+        ++destroyed;
+    }
+    size_t destroyed = 0;
+};
+
+
+TEST_CASE("Refcounted pointer - empty", "[Refcounted pointer]") {
+    rc_ptr<managed> ptr;
+    REQUIRE(!ptr);
+    REQUIRE(ptr.use_count() == 0);
+    REQUIRE(ptr.unique() == false);
+    REQUIRE(ptr.get() == nullptr);
+}
+
+
+TEST_CASE("Refcounted pointer - unique", "[Refcounted pointer]") {
+    managed object;
+    {
+        rc_ptr ptr(&object);
+        REQUIRE(ptr);
+        REQUIRE(ptr.use_count() == 1);
+        REQUIRE(ptr.unique() == true);
+        REQUIRE(ptr.get() == &object);
+    }
+    REQUIRE(object.destroyed == 1);
+}
+
+
+TEST_CASE("Refcounted pointer - multiple", "[Refcounted pointer]") {
+    managed object;
+    {
+        rc_ptr ptr1(&object);
+        rc_ptr ptr2(&object);
+
+        REQUIRE(ptr1.use_count() == 2);
+        REQUIRE(ptr1.unique() == false);
+        REQUIRE(ptr1.get() == &object);
+
+        REQUIRE(ptr2.use_count() == 2);
+        REQUIRE(ptr2.unique() == false);
+        REQUIRE(ptr2.get() == &object);
+
+        REQUIRE(ptr1 == ptr2);
+    }
+    REQUIRE(object.destroyed == 1);
+}
+
+
+TEST_CASE("Refcounted pointer - move construct", "[Refcounted pointer]") {
+    managed object;
+    {
+        rc_ptr ptr(&object);
+        rc_ptr copy(std::move(ptr));
+
+        REQUIRE(copy);
+        REQUIRE(copy.use_count() == 1);
+        REQUIRE(copy.unique() == true);
+        REQUIRE(copy.get() == &object);
+    }
+    REQUIRE(object.destroyed == 1);
+}
+
+
+TEST_CASE("Refcounted pointer - copy construct", "[Refcounted pointer]") {
+    managed object;
+    {
+        rc_ptr ptr(&object);
+        rc_ptr copy(ptr);
+
+        REQUIRE(copy);
+        REQUIRE(copy.use_count() == 2);
+        REQUIRE(copy.unique() == false);
+        REQUIRE(copy.get() == &object);
+    }
+    REQUIRE(object.destroyed == 1);
+}
+
+
+TEST_CASE("Refcounted pointer - move assign", "[Refcounted pointer]") {
+    managed object;
+    {
+        rc_ptr ptr(&object);
+        rc_ptr<managed> copy;
+        copy = std::move(ptr);
+
+        REQUIRE(copy);
+        REQUIRE(copy.use_count() == 1);
+        REQUIRE(copy.unique() == true);
+        REQUIRE(copy.get() == &object);
+    }
+    REQUIRE(object.destroyed == 1);
+}
+
+
+TEST_CASE("Refcounted pointer - copy assign", "[Refcounted pointer]") {
+    managed object;
+    {
+        rc_ptr ptr(&object);
+        rc_ptr<managed> copy;
+        copy = ptr;
+
+        REQUIRE(copy);
+        REQUIRE(copy.use_count() == 2);
+        REQUIRE(copy.unique() == false);
+        REQUIRE(copy.get() == &object);
+    }
+    REQUIRE(object.destroyed == 1);
+}
+
+
+TEST_CASE("Refcounted pointer - dereference", "[Refcounted pointer]") {
+    managed object;
+    object.destroyed = 10;
+    rc_ptr ptr(&object);
+    REQUIRE(ptr->destroyed == 10);
+    REQUIRE((*ptr).destroyed == 10);
+    REQUIRE(ptr.get()->destroyed == 10);
+
+}
